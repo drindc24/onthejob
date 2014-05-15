507b22f74d4e8979e7ccb3bdb11a3e47a4e890d5
Aldrin dela Cruz <aldrin@aldrin-Aspire-4745G.(none)>
2014-05-15 23:03:54 +0800

    Updated asset pipeline order of files.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
-//= require_tree ./plugins/
-
+//= require bootstrap
+//= require sb-admin
+//= require plugins/dataTables/dataTables.bootstrap.js
+//= require plugins/dataTables/jquery.dataTables.js
