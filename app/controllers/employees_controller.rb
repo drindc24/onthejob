737b04ed07c94c4d9d44dfd929e4793b64bdf083
Aldrin dela Cruz <aldrin@aldrin-Aspire-4745G.(none)>
2014-05-20 14:40:29 +0800

    Added show action for employees controller.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,16 +1,15 @@
 class EmployeesController < ApplicationController
   before_filter :authenticate_employee!
-  before_filter :check_if_logged_in, :only => :index
 
   def index
-    @employees = Employee.all
+    @company = current_employee.company
+    @employees = @company.employees
   end
 
   def show
-    @employee = Employee.find(params[:id])
-  end
-
-  def check_if_logged_in
-    redirect_to new_employee_session_path unless current_employee
+    if params[:id]
+    else
+      @employee = current_employee
+    end
   end
 end
