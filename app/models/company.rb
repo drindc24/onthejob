fc8c23a7bd5b5a9934b787cbcee932d3dce0b360
Aldrin dela Cruz <aldrin@aldrin-Aspire-4745G.(none)>
2014-05-23 20:23:34 +0800

    Added new validation for company model. (for departments which is a new model)

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,7 @@ class Company < ActiveRecord::Base
   attr_accessible :business_address, :business_phone, :date_founded, :fax_number, :full_name, :slug, :status
 
   has_many :employees, :dependent => :destroy
+  has_many :departments, :dependent => :destroy
 
   def is_employee?(employee)
     self.employees.include?(employee)
