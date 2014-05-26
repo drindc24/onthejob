c2d3701b55cbca252b781f33176edda7f531810d
Aldrin dela Cruz <aldrin@aldrin-Aspire-4745G.(none)>
2014-05-27 00:42:40 +0800

    Added panel view helper.

--- app/helpers/employees_helper.rb
+++ app/helpers/employees_helper.rb
@@ -1,2 +1,5 @@
 module EmployeesHelper
+  def panel(class_name, value, label, link)
+     render :partial => "panel_view", :locals => { :class_name => class_name, :value => value, :label => label, :link => link }
+  end
 end
